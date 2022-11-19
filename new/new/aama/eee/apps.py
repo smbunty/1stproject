ff79a50f29aef7aa4ad109ef0b8f3f9cce9a365c
smbunty <118590024+smbunty@users.noreply.github.com>
2022-11-19 12:29:15 +0530

    Revert "sourabh"
    This reverts commit c7c5974565c44e3f0cd4a0aba51aff57137830cd.

--- new/new/aama/eee/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EeeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'eee'
--- new/new/aama/eee/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Employe
-
-class EmpForm(forms.ModelForm):
-    class Meta:
-        model = Employe
-        fields = "__all__"
